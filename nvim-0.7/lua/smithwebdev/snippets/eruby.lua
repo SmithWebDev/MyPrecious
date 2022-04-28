91d00a4c50d66d181d5bcb49cbab712aff59c265
SmithWebDev <smithwebdd@gmail.com>
2022-04-28 03:00:38 -0400

    updated eruby snippets

--- nvim-0.7/lua/smithwebdev/snippets/eruby.lua
+++ nvim-0.7/lua/smithwebdev/snippets/eruby.lua
@@ -4,41 +4,41 @@ return {
     name = 'Link to with class',
     dscr = 'Link_to tag with added class option / optional delete action'
   }, fmt(
-    [[<%= link_to '{}', {}_path{},{} class: '{}' %>]],
-    { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
+      [[<%= link_to '{}', {}_path{},{} class: '{}' %>]],
+      { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
   s({
     trig = 'btc',
     name = 'Button to with class',
     dscr = 'Button_to tag with added class option / optional delete action'
   }, fmt(
-    [[<%= button_to '{}', {}_path{},{} class: '{}' %>]],
-    { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
+      [[<%= button_to '{}', {}_path{},{} class: '{}' %>]],
+      { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
   s({
     trig = 'tftb',
     name = 'turbo_frame_tag',
     dscr = 'Create turbo frame tag',
   },
     {
-    t('<%= turbo_frame_tag '),
-    i(1),
-    t(' do %>'),
-    f(function(_, snip)
-      return snip.env.TM_SELECTED_TEXT[1] or {}
-    end, {}),
-    t('<% end %>'),
-    i(0)
-  }
+      t('<%= turbo_frame_tag '),
+      i(1),
+      t(' do %>'),
+      f(function(_, snip)
+        return snip.env.TM_SELECTED_TEXT[1] or {}
+      end, {}),
+      t('<% end %>'),
+      i(0)
+    }
   ),
   s({
     trig = 'tft',
     name = 'turbo_frame_tag',
     dscr = 'Create turbo frame tag',
   }, {
-    t('<%= turbo_frame_tag '),
-    c(1, { t(''), { t('dom_id('), i(1), t(')') } }),
-    t(' do %>'),
-    i(0)
-  }),
+      t('<%= turbo_frame_tag '),
+      c(1, { t(''), { t('dom_id('), i(1), t(')') } }),
+      t(' do %>'),
+      i(0)
+    }),
   s({
     trig = 'dtframe',
     name = 'data turbo frame',
@@ -57,52 +57,53 @@ return {
   },
     fmt([[<%= turbo_stream{}{} {}{}{} %>]],
       { c(1, {
-      t('.'),
-      t('_from.'),
-      t('')
-    }),
-    c(2, {
-      t(''),
-      -- Remove a Turbo Frame
-      t('remove'),
-      t('remove_all'),
+        t('.'),
+        t('_from.'),
+        t('')
+      }),
+        c(2, {
+          t(''),
 
-      -- Insert a Turbo Frame at the beginning/end of a list
-      t('append'),
-      t('append_all'),
-      t('prepend'),
-      t('prepend_all'),
+          -- Remove a Turbo Frame
+          t('remove'),
+          t('remove_all'),
 
-      -- Insert a Turbo Frame before/after another Turbo Frame
-      t('before'),
-      t('before_all'),
-      t('after'),
-      t('after_all'),
+          -- Insert a Turbo Frame at the beginning/end of a list
+          t('append'),
+          t('append_all'),
+          t('prepend'),
+          t('prepend_all'),
 
-      -- Replace or update the content of a Turbo Frame
-      t('replace'),
-      t('replace_all'),
-      t('update'),
-      t('update_all'),
+          -- Insert a Turbo Frame before/after another Turbo Frame
+          t('before'),
+          t('before_all'),
+          t('after'),
+          t('after_all'),
 
-      t('action'),
-      t('action_all'),
-    }),
-    i(3),
-    c(4, {
-      t(''),
-      { t(', partial: \''),
-        i(1),
-        t('\' ') },
-    }),
-    c(5, {
-      t(''),
-      { t(', locals: { '),
-        i(1), t(': @'), rep(1),
-        t(' }')
-      }
-    })
-    })
+          -- Replace or update the content of a Turbo Frame
+          t('replace'),
+          t('replace_all'),
+          t('update'),
+          t('update_all'),
+
+          t('action'),
+          t('action_all'),
+        }),
+        i(3),
+        c(4, {
+          t(''),
+          { t(', partial: \''),
+            i(1),
+            t('\' ') },
+        }),
+        c(5, {
+          t(''),
+          { t(', locals: { '),
+            i(1), t(': @'), rep(1),
+            t(' }')
+          }
+        })
+      })
   ),
 },
-    print("eruby snippets attached")
+print("eruby snippets attached")

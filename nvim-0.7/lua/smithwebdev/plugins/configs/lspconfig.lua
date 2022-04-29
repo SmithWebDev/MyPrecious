dfe49b5ee744c8ae1cf66cefe513ae02b60c64cd
SmithWebDev <smithwebdd@gmail.com>
2022-04-29 13:44:15 -0400

    attempting to update LSPCONFIG docs... failing miserably *WIP*

--- nvim-0.7/lua/smithwebdev/plugins/configs/lspconfig.lua
+++ nvim-0.7/lua/smithwebdev/plugins/configs/lspconfig.lua
@@ -8,54 +8,70 @@ M.plugin = {
   },
 
   config = function()
-    local lspinstaller = require('nvim-lsp-installer')
+    local lspconfig = require('lspconfig')
+    local lspinstaller = require('nvim-lsp-installer').setup()
     local on_attach = require('smithwebdev.lsp').on_attach
     local capabilities = require('smithwebdev.lsp').get_capabilities()
 
-    lspinstaller.on_server_ready(function(server)
-      local opts = {
+    --lspinstaller.on_server_ready(function(server)
+    --  local opts = {
+    --    on_attach = on_attach,
+    --    capabilities = capabilities,
+    --  }
+
+    --  if server.name == 'sumneko_lua' then
+    --    local runtime_path = vim.split(package.path, ';')
+    --    table.insert(runtime_path, 'lua/?.lua')
+    --    table.insert(runtime_path, 'lua/?/init.lua')
+
+    --    opts.settings = {
+    --      Lua = {
+    --        runtime = {
+    --          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+    --          version = 'LuaJIT',
+    --          -- Setup your lua path
+    --          path = runtime_path,
+    --        },
+    --        diagnostics = {
+    --          -- Get the language server to recognize the `vim` global
+    --          globals = {'vim'},
+    --        },
+    --        workspace = {
+    --          -- Make the server aware of Neovim runtime files
+    --          library = vim.api.nvim_get_runtime_file("", true),
+    --        },
+    --        -- Do not send telemetry data containing a randomized but unique identifier
+    --        telemetry = {
+    --          enable = false,
+    --        },
+    --      },
+    --    }
+    --  end
+    --  if server.name == 'solargraph' then
+    --    opts.settings = {
+    --      solargraph = {
+    --        formatting = true
+    --      }
+    --    }
+    --  end
+
+    --  server:setup(opts)
+    --end)
+
+    local server = {'solargraph'}
+    for _, lsp in ipairs(server) do
+      require('lspconfig')[lsp].setup {
         on_attach = on_attach,
         capabilities = capabilities,
+        root_dir = function() return vim.loop.cwd() end
       }
+    end
 
-      if server.name == 'sumneko_lua' then
-        local runtime_path = vim.split(package.path, ';')
-        table.insert(runtime_path, 'lua/?.lua')
-        table.insert(runtime_path, 'lua/?/init.lua')
 
-        opts.settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
-              -- Setup your lua path
-              path = runtime_path,
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = {'vim'},
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-          },
-        }
-      end
-      if server.name == 'solargraph' then
-        opts.settings = {
-          solargraph = {
-            formatting = true
-          }
-        }
-      end
-
-      server:setup(opts)
-    end)
+    require('lspconfig').sumneko_lua.setup({
+      on_attach = on_attach,
+      capabilities = capabilities
+    })
   end
 }
 

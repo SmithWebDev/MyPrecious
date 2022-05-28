447586084acc2d7cac8f9b4025f3ab274059dda2
SmithWebDev <smithwebdd@gmail.com>
2022-05-28 13:20:20 -0400

    created snippets for JS

--- nvim-0.7/lua/smithwebdev/snippets/javascript.lua
+++ nvim-0.7/lua/smithwebdev/snippets/javascript.lua
@@ -13,5 +13,12 @@ return {
   },
     fmt([[import {} from "{}";]], { i(2), i(1) })
   ),
+  s({
+    trig = 'req',
+    name = 'require',
+    dscr = 'require syntax for javascript'
+  },
+    { t("require('"), i(1), t("\')") }
+  ),
 },
     print("javascript snippets attached")

d337de24f192fbc3e8011b9cc1d7046723c94ef8
SmithWebDev <smithwebdd@gmail.com>
2022-05-30 23:50:53 -0400

    updated snippets files for eruby and lua

--- nvim-0.7/lua/smithwebdev/snippets/eruby.lua
+++ nvim-0.7/lua/smithwebdev/snippets/eruby.lua
@@ -1,13 +1,23 @@
 return {
 
+
   -- Link_to Helper snippet
   s({
     trig = 'ltc',
     name = 'Link to with class',
     dscr = 'Link_to tag with added class option / optional delete action'
   }, fmt(
-      [[<%= link_to '{}', {}_path{},{} class: '{}' %>]],
-      { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
+    [[<%= link_to '{}', {}_path{},{} class: '{}' %>]],
+    { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
+
+  -- Button_to Helper snippet
+  --s({
+  --  trig = 'btc',
+  --  name = 'Button to with class',
+  --  dscr = 'Button_to tag with added class option / optional delete action'
+  --}, fmt(
+  --  [[<%= button_to '{}', {}_path{},{} {} %>]],
+  --  { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), c(5, { t(''), { t('class: \''), i(1), t('\'') } }) })),
 
   -- Button_to Helper snippet
   s({
@@ -15,8 +25,26 @@ return {
     name = 'Button to with class',
     dscr = 'Button_to tag with added class option / optional delete action'
   }, fmt(
-      [[<%= button_to '{}', {}_path{},{} class: '{}' %>]],
-      { i(1), i(2), i(3), c(4, { t(''), t(' method: :delete,') }), i(5) })),
+    [[<%= button_to '{}'{}{}{}%>]], {
+    i(1),
+    c(2, {
+      { t(', '), i(1), t('_path'), t(' ') },
+      { t(', '), i(1), t('_path('), i(2), t(')') },
+    }),
+    c(3, {
+      { t(', destroy_user_session_path, method: :delete'), t(' ') },
+      { t(', '), i(1), t('_path('), i(2), t('), method: :delete'), t(' ') },
+      { t(', '), i(1), t(', method: :delete'), t(' ') },
+      { t(', destroy_user_session_path, data: {"turbo-method": :delete}'), t(' ') },
+      { t(', '), i(1), t('_path('), i(2), t('), data: {"turbo-method": :delete}'), t(' ') },
+      { t(', '), i(1), t(', data: {"turbo-method": :delete}'), t(' ') },
+    }),
+    c(4, {
+      t(' '),
+      { t(', class: \''), i(1), t('\'') }
+    }),
+
+  })),
 
   -- Turbo Frame Tag helper block *WIP*
   s({
@@ -27,13 +55,13 @@ return {
     <%= turbo_frame_tag {} do %>
       {}
     <% end %>
-    ]],{
-        c(1, {{i(1)}, t('\'_top\''),  {t('dom_id('), i(1), t(') ') }}),
-        f(function(_, snip)
-          return snip.env.TM_SELECTED_TEXT[1] or {}
-        end, {})
-      }
-    )
+    ]], {
+    c(1, { { i(1) }, t('\'_top\''), { t('dom_id('), i(1), t(') ') } }),
+    f(function(_, snip)
+      return snip.env.TM_SELECTED_TEXT[1] or {}
+    end, {})
+  }
+  )
   ),
 
   -- Turbo Frame Tag helper snippet
@@ -42,24 +70,24 @@ return {
     name = 'turbo_frame_tag',
     dscr = 'Create turbo frame tag',
   }, fmt(
-      [[<%= turbo_frame_tag {}{}]],
+    [[<%= turbo_frame_tag {}{}]],
+    {
+    c(1, {
+      i(1),
       {
-        c(1,{
+        t('dom_id('),
+        c(1, {
           i(1),
-          {
-            t('dom_id('),
-            c(1,{
-              i(1),
-              { t('dom_id('), i(1), t(')') }
-            }),
-            t(')')
-          },
+          { t('dom_id('), i(1), t(')') }
         }),
-        c(2,{
-          t(' %>'),
-          t(' do %>')
-        })
+        t(')')
+      },
+    }),
+    c(2, {
+      t(' %>'),
+      t(' do %>')
     })
+  })
   ),
 
   -- Data Turbo Frame attribute snippet
@@ -71,10 +99,10 @@ return {
   {}
   data: {{ turbo_frame: {} }}{}
   ]], {
-      c(1, { t(''), t(', ') }),
-      c(2, { t('\'_top\''), i(1), { t('dom_id('), i(1), t(') ') } }),
-      c(3, { t(''), t(', ') })
-    })
+    c(1, { t(''), t(', ') }),
+    c(2, { t('\'_top\''), i(1), { t('dom_id('), i(1), t(') ') } }),
+    c(3, { t(''), t(', ') })
+  })
   ),
 
   s({
@@ -82,10 +110,10 @@ return {
     name = 'data turbo frame',
     dscr = 'Data attr for turbo frames'
   }, fmt([[{} form: {{data: {{ turbo_frame: {} }}{} }}]], {
-      c(1, { t(''), t(', ') }),
-      c(2, { t('\'_top\''), i(1), { t('dom_id('), i(1), t(') ') } }),
-      c(3, { t(''), t(', ') })
-    })
+    c(1, { t(''), t(', ') }),
+    c(2, { t('\'_top\''), i(1), { t('dom_id('), i(1), t(') ') } }),
+    c(3, { t(''), t(', ') })
+  })
   ),
 
   -- Turbo Stream helper tag snippet
@@ -94,88 +122,88 @@ return {
     name = 'turbo stream',
     dscr = 'Inserts turbo stream tag with methods, partial, locals support'
   }, fmt([[<%= turbo_stream{}{} {}{}{} %>]],
-      { c(1, {
-        t('.'),
-        t('_from'),
-        t('_from.'),
-        t('')
-      }),
-        c(2, {
-          t(''),
+    { c(1, {
+    t('.'),
+    t('_from'),
+    t('_from.'),
+    t('')
+  }),
+  c(2, {
+    t(''),
 
-          t('action'),
-          t('action_all'),
+    t('action'),
+    t('action_all'),
 
-          -- Insert a Turbo Frame before/after another Turbo Frame
-          t('after'),
-          t('after_all'),
-          t('before'),
-          t('before_all'),
+    -- Insert a Turbo Frame before/after another Turbo Frame
+    t('after'),
+    t('after_all'),
+    t('before'),
+    t('before_all'),
 
-          -- Insert a Turbo Frame at the beginning/end of a list
-          t('append'),
-          t('append_all'),
-          t('prepend'),
-          t('prepend_all'),
+    -- Insert a Turbo Frame at the beginning/end of a list
+    t('append'),
+    t('append_all'),
+    t('prepend'),
+    t('prepend_all'),
 
-          -- Replace or update the content of a Turbo Frame
-          t('replace'),
-          t('replace_all'),
-          t('update'),
-          t('update_all'),
+    -- Replace or update the content of a Turbo Frame
+    t('replace'),
+    t('replace_all'),
+    t('update'),
+    t('update_all'),
 
-          -- Remove a Turbo Frame
-          t('remove'),
-          t('remove_all'),
-        }),
-        i(3),
-        c(4, {
-          t(''),
-          { t(', partial: \''),
-            i(1),
-            t('\' ') },
-        }),
-        c(5, {
-          t(''),
-          { t(', locals: { '),
-            i(1), t(': @'), rep(1),
-            t(' }')
-          },
-          t(' do')
-        })
-      })
+    -- Remove a Turbo Frame
+    t('remove'),
+    t('remove_all'),
+  }),
+  i(3),
+  c(4, {
+    t(''),
+    { t(', partial: \''),
+      i(1),
+      t('\' ') },
+  }),
+  c(5, {
+    t(''),
+    { t(', locals: { '),
+      i(1), t(': @'), rep(1),
+      t(' }')
+    },
+    t(' do')
+  })
+  })
   ),
   s({
     trig = 'aftcommit',
     name = 'After Commit Actions',
     dscr = 'Broadcast transaction shortcuts'
   }, fmt(
-      [[after_{}_commit -> {{ {} {} {} {} }}]],
-      {c(1,{
-        t('save'),
-        t('create'),
-        t('update'),
-        t('destroy')
-      }),
-        sn(2, {
-          t('bactions')
-        }),
-        c(3,{
-          {t('partial: \''),
-            i(1),
-            t('\',')
-          },
-          t('')
-        }),
-        c(4, {
-          {t('locals: {'), i(1), t('}: {'), i(2), t('},')},
-          t('')
-        }),
-        c(5, {
-          {t('target: \''), i(1), t('\',')},
-          t('')
-        }),
-      })
+    [[after_{}_commit -> {{ {} {} {} {} }}]],
+    { c(1, {
+    t('save'),
+    t('create'),
+    t('update'),
+    t('destroy')
+  }),
+  sn(2, {
+    t('bactions')
+  }),
+  c(3, {
+    { t('partial: \''),
+      i(1),
+      t('\',')
+    },
+    t('')
+  }),
+  c(4, {
+    { t('locals: {'), i(1), t('}: {'), i(2), t('},') },
+    t('')
+  }),
+  c(5, {
+    { t('target: \''), i(1), t('\',') },
+    t('')
+  }),
+  })
   ),
   s({
     trig = 'bactions',
@@ -184,31 +212,31 @@ return {
   }, fmt([[
   broadcast{} '{}'{}
   ]], {
-        c(1,{
-          t('s_to'),
-          t('_action_to'),
-          t('_action_later_to'),
-          t('_after_to'),
-          t('_append_to'),
-          t('_append_later_to'),
-          t('_before_to'),
-          t('_before_later_to'),
-          t('_prepend_to'),
-          t('_prepend_later_to'),
-          t('_update_to'),
-          t('_update_later_to'),
-          t('_remove_to'),
-          t('_render_to'),
-          t('_render_later_to'),
-          t('_replace_to'),
-          t('_replace_later_to'),
-        }),
-        i(2),
-        c(3,{
-          t(''),
-          t(', ')
-        })
-      })
+    c(1, {
+      t('s_to'),
+      t('_action_to'),
+      t('_action_later_to'),
+      t('_after_to'),
+      t('_append_to'),
+      t('_append_later_to'),
+      t('_before_to'),
+      t('_before_later_to'),
+      t('_prepend_to'),
+      t('_prepend_later_to'),
+      t('_update_to'),
+      t('_update_later_to'),
+      t('_remove_to'),
+      t('_render_to'),
+      t('_render_later_to'),
+      t('_replace_to'),
+      t('_replace_later_to'),
+    }),
+    i(2),
+    c(3, {
+      t(''),
+      t(', ')
+    })
+  })
   ),
   s({
     trig = 'bact',
@@ -217,55 +245,55 @@ return {
   }, fmt([[
   broadcast_{}
   ]], {
-        c(1,{
-          t('to'),
-          t('action_to'),
-          t('action_later_to'),
-          t('after_to'),
-          t('append_to'),
-          t('append_later_to'),
-          t('before_to'),
-          t('before_later_to'),
-          t('prepend_to'),
-          t('prepend_later_to'),
-          t('update_to'),
-          t('update_later_to'),
-          t('remove_to'),
-          t('render_to'),
-          t('render_later_to'),
-          t('replace_to'),
-          t('replace_later_to'),
-          t('stream_to'),
-        })
-      })
+    c(1, {
+      t('to'),
+      t('action_to'),
+      t('action_later_to'),
+      t('after_to'),
+      t('append_to'),
+      t('append_later_to'),
+      t('before_to'),
+      t('before_later_to'),
+      t('prepend_to'),
+      t('prepend_later_to'),
+      t('update_to'),
+      t('update_later_to'),
+      t('remove_to'),
+      t('render_to'),
+      t('render_later_to'),
+      t('replace_to'),
+      t('replace_later_to'),
+      t('stream_to'),
+    })
+  })
   ),
   s({
     trig = 'dat',
     name = 'Data Attribute',
     dscr = 'insert data attribute into markup'
   }, fmt([[data{}]], {
+    c(1, {
+      t(''),
+      t(': '),
       c(1, {
-        t(''),
-        t(': '),
-        c(1,{
-          {t('controller=\''), i(1), t('\'')},
-          {t('action=\''),i(1, 'data_action'), t('->'), i(2, 'stim_controller'), t('#'), i(3, 'stim_action'),t('\'') }}
-        )})
-    })
+        { t('controller=\''), i(1), t('\'') },
+        { t('action=\''), i(1, 'data_action'), t('->'), i(2, 'stim_controller'), t('#'), i(3, 'stim_action'), t('\'') } }
+      ) })
+  })
   ),
   s({
     trig = 'dc',
     name = 'Data Controller',
     dscr = 'adds Data controller attribute'
   },
-    fmt([[data-controller = '{}']], {i(1)})
+    fmt([[data-controller = '{}']], { i(1) })
   ),
   s({
     trig = 'da',
     name = 'Data Action',
     dscr = 'adds Data action attribute'
   },
-    fmt([[data-action='{}->{}#{}']], {i(1, 'DataAction'), i(2, 'StimulusController'), i(3, 'StimulusAction')})
+    fmt([[data-action='{}->{}#{}']], { i(1, 'DataAction'), i(2, 'StimulusController'), i(3, 'StimulusAction') })
   ),
   s({
     trig = 'ren',
@@ -279,4 +307,4 @@ return {
     })
   ),
 },
-  print("eruby snippets attached")
+    print("eruby snippets attached")
--- nvim-0.7/lua/smithwebdev/snippets/lua.lua
+++ nvim-0.7/lua/smithwebdev/snippets/lua.lua
@@ -1,7 +1,7 @@
 local same = function(index)
   return f(function(arg)
     return arg[1]
-  end, {index})
+  end, { index })
 end
 
 return {
@@ -10,24 +10,24 @@ return {
     fmt(
       [[local {} = require "{}"]],
       { f(function(import_name)
-        local parts = vim.split(import_name[1][1], ".", true)
-        return parts[#parts] or ''
-      end, {1}), i(1)}
+      local parts = vim.split(import_name[1][1], ".", true)
+      return parts[#parts] or ''
+    end, { 1 }), i(1) }
     )
   ),
   --s("req", fmt([[local {} = require('{}')]], { i(1), rep(1)})),
   s('keymap', fmt([[
   {}noremap('{}',  '{}', {{ desc = '{}'}})
   ]], {
-      c(1, {
-        t('i'),
-        t('n'),
-        t('v')
-      }),
-      i(2),
-      i(3),
-      i(4)
-    })
+    c(1, {
+      t('i'),
+      t('n'),
+      t('v')
+    }),
+    i(2),
+    i(3),
+    i(4)
+  })
   ),
   s('pcreate', fmt([[
      local M = {{}}
@@ -41,7 +41,7 @@ return {
      }}
 
      return M]],
-    {i(1), i(2)})),
+    { i(1), i(2) })),
   --s('auc', {
   --  t('vim.api.nvim_create_autocmd({ '),
   --  i(1),
@@ -50,12 +50,41 @@ return {
   --  i()
   --})
   --}),
-  s('aug', fmt("vim.api.nvim_create_autogroup('{}', {{ clear = true }})", {i(1)})),
+  s('aug', fmt("vim.api.nvim_create_autogroup('{}', {{ clear = true }})", { i(1) })),
   s('auc', fmt([[
       vim.api.nvim_create_autocmd('{}', {{
         pattern = '{}',
         command = '{}',
         group = '{}'
       }})
-    ]], {i(1), i(2), i(3), i(4)})),
+    ]], { i(1), i(2), i(3), i(4) })),
+
+  --rails class erb
+  s({
+    trig = 'cla',
+    name = 'class',
+    dscr = 'snippet for class option snippet in erb'
+  }, fmt([[
+    c({}, {{
+      t(''),
+      {{ t('class: \''), i(1), t('\'') }}
+    }})
+  ]], { i(1) })
+  ),
+  s({
+    trig = 'dest',
+    name = 'destroy',
+    dscr = 'snippet for destroy method snippet in erb links'
+  },
+    fmt([[
+      c({}, {{
+      {{ t(', destroy_user_session_path, method: :delete'), t(' ') }},
+      {{ t(', '), i(1), t('_path('),i(2),t('), method: :delete'), t(' ') }},
+      {{ t(', '), i(1), t(', method: :delete'), t(' ') }},
+      {{ t(', destroy_user_session_path, data: {{"turbo-method": :delete}}'), t(' ') }},
+      {{ t(', '), i(1), t('_path('),i(2),t('), data: {{"turbo-method": :delete}}'), t(' ') }},
+      {{ t(', '), i(1), t(', data: {{"turbo-method": :delete}}'), t(' ') }},
+      }})
+    ]], { i(1) })
+  ),
 }

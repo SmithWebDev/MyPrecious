e1df22c509915a2968b2b6598885d5e8d156c4ce
Darrick Smith <smithwebdd@gmail.com>
2022-06-20 14:21:24 -0400

    updated colorscheme

--- nvim-0.7/lua/smithwebdev/theme.lua
+++ nvim-0.7/lua/smithwebdev/theme.lua
@@ -1,6 +1,7 @@
 --local theme = 'mirodark'
 --local theme = 'tokyonight'
-local theme = 'moonlight'
+--local theme = 'moonlight'
+local theme = 'petrel'
 --local theme = 'gloombuddy'
 
 local ok, err = pcall(vim.cmd, 'colorscheme ' .. theme)

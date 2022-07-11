eabaf5bea136d3afcf4206741e875a5e71662aff
Darrick Smith <smithwebdd@gmail.com>
2022-07-11 10:00:31 -0400

    disabled rufo-vim plugin

--- nvim-0.7/lua/smithwebdev/plugins/configs/rails.lua
+++ nvim-0.7/lua/smithwebdev/plugins/configs/rails.lua
@@ -12,7 +12,7 @@ M.plugin = {
     'tpope/vim-dispatch', --, https://github.com/tpope/vim-dispatch
     'tpope/vim-projectionist', --, https://github.com/tpope/vim-projectionist
     'jgdavey/vim-blockle', --, https://github.com/gdavey/vim-blockle
-    'ruby-formatter/rufo-vim', --, https://github.com/ruby-formatter/rufo-vim
+    --'ruby-formatter/rufo-vim', --, https://github.com/ruby-formatter/rufo-vim
   },
 
   config = function()

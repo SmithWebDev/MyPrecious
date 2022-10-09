d20a3a17203aeba515ac28fa10381a0a6adb4eed
Darrick Smith <smithwebdd@gmail.com>
2022-10-09 15:12:11 -0400

    Added configuration file for plugins, auto clone/install packer

---
+++ nvim-0.8/lua/smithwebdev/plugins/init.lua
@@ -0,0 +1,56 @@
+local install_path = os.getenv('HOME') .. '/.local/share/nvim/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  local plugin_repo = 'https://www.github.com/wbthomason/packer.nvim'
+  local cli = string.format('!git clone %s %s', plugin_repo, install_path)
+  vim.cmd(cli)
+end
+
+vim.cmd 'packadd packer.nvim'
+local packer = require 'packer'
+
+-- Use your local data path and not plugin/
+-- this is `~/.local/share/nvim/site/plugin/packer_compiled.lua` in WSL/Linux and MacOS
+packer.init {
+  package_root = string.format('%s/site/pack', vim.fn.stdpath('data')),
+  compile_path = string.format('%s/site/plugin/packer_compiled.lua', vim.fn.stdpath('data')),
+  max_jobs = 30,
+}
+
+local plugins = {
+}
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+
+  for _, v in pairs(plugins) do
+    use(require('smithwebdev.plugins.configs.' .. v).plugin)
+  end
+end)
+
+-- Autocmd to match the path in your config that is in
+-- lua/smithwebdev/plugins/*.lua or lua/smithwebdev/plugins/configs/*.lua
+-- and then create a packer compile file whenever you :w in those files
+local packer_group = vim.api.nvim_create_augroup('packer_user_events', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = packer_group,
+
+  pattern = '*.lua',
+
+  callback = function(event)
+    local found = event.file:find('.*smithwebdev/plugins.*') ~= nil
+
+    if found then
+      --vim.cmd 'PackerSync'
+      vim.cmd 'PackerCompile'
+    end
+  end,
+})
+
+local u = require 'smithwebdev.core.utils'
+local nnoremap = u.nnoremap
+
+nnoremap('<leader>PS' , ':PackerSync<cr>'    , { desc = "Packer Sync" })
+nnoremap('<leader>PC' , ':PackerCompile<cr>' , { desc = "Packer Compile" })
+
+print('Plugins connected...')

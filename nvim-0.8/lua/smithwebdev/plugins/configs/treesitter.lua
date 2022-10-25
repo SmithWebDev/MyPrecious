ba5ab40286ed5484d684bb3880b6c2c51438cb49
Darrick Smith <smithwebdd@gmail.com>
2022-10-25 10:44:11 -0400

    Updated treesitter to use treesitter-context

--- nvim-0.8/lua/smithwebdev/plugins/configs/treesitter.lua
+++ nvim-0.8/lua/smithwebdev/plugins/configs/treesitter.lua
@@ -4,15 +4,15 @@ M.plugin = {
   'nvim-treesitter/nvim-treesitter',
 
   requires = {
-    {'nvim-treesitter/playground',                  after = {'nvim-treesitter', 'tree-sitter-query'}}, --https://github.com/nvim-treesitter/playground
+    {'nvim-treesitter/playground',                  after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/nvim-treesitter/playground
     {'RRethy/nvim-treesitter-endwise',              after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/RRethy/nvim-treesitter-endwise
-    {'nvim-treesitter/nvim-treesitter-refactor',    after = {'nvim-treesitter', 'tree-sitter-query'}},
+    {'nvim-treesitter/nvim-treesitter-refactor',    after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/nvim-treesitter/nvim-treesitter-refactor
     {'nvim-treesitter/nvim-treesitter-textobjects', after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     {'RRethy/nvim-treesitter-textsubjects',         after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/RRethy/nvim-treesitter-textsubjects
-    {'nvim-treesitter/nvim-treesitter-context',     after = {'nvim-treesitter', 'tree-sitter-query'}}, --https://github.com/nvim-treesitter/nvim-treesitter-context
     {'p00f/nvim-ts-rainbow',                        after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/p00f/nvim-ts-rainbow
     {'ziontee113/syntax-tree-surfer',               after = {'nvim-treesitter', 'tree-sitter-query'}}, -- https://github.com/ziontee113/syntax-tree-surfer
     {'nvim-treesitter/tree-sitter-query',           after = 'nvim-treesitter'},                        -- https://github.com/nvim-treesitter/tree-sitter-query
+    'nvim-treesitter/nvim-treesitter-context',                                                         -- https://github.com/nvim-treesitter/nvim-treesitter-context
   },
 
   run = function()
@@ -113,24 +113,28 @@ M.plugin = {
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-    --require('treesitter-context').setup {
-    --  enable = true,
-    --  max_lines = 0,
-    --  patterns = {
-    --    default = {
-    --      'class',
-    --      'function',
-    --      'method',
-    --      'for',
-    --      'while',
-    --      'if',
-    --      'switch',
-    --      'case',
-    --      'def',
-    --    },
-    --  },
-    --  zindex = 20,
-    --}
+    require('treesitter-context').setup {
+      enable = true,
+      max_lines = 0,
+      trim_scope = 'outer',
+      min_window_height = 0,
+      patterns = {
+        default = {
+          'class',
+          'function',
+          'method',
+          'for',
+          'while',
+          'if',
+          'switch',
+          'case',
+          'def',
+        },
+      },
+      zindex = 20,
+      mode = 'cursor',
+      separator = nil
+    }
   end
 }
 

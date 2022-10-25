89b62f3c90a3689afb3a4fb61b5536ee9e028c7a
Darrick Smith <smithwebdd@gmail.com>
2022-10-25 12:30:46 -0400

    Updated LSPSaga

--- nvim-0.8/lua/smithwebdev/plugins/configs/lspsaga.lua
+++ nvim-0.8/lua/smithwebdev/plugins/configs/lspsaga.lua
@@ -1,63 +1,169 @@
 local M = {}
 
 M.plugin = {
-  'tami5/lspsaga.nvim', -- https://github.com/tami5/lspsaga.nvim
+  'glepnir/lspsaga.nvim', -- https://github.com/glepnir/lspsaga.nvim
 
   config = function()
-
     local lspsaga = require 'lspsaga'
-    lspsaga.setup {
-      --
-      -- defaults ...
-      debug = false,
-      use_saga_diagnostic_sign = true,
-
-      -- diagnostic sign
-      error_sign = "",
-      warn_sign = "",
-      hint_sign = "",
-      infor_sign = "",
-      diagnostic_header_icon = "   ",
-
-      -- code action title icon
-      code_action_icon = " ",
-      code_action_prompt = {
+    lspsaga.init_lsp_saga({
+      -- Options with default value
+      -- "single" | "double" | "rounded" | "bold" | "plus"
+      border_style = "single",
+      --the range of 0 for fully opaque window (disabled) to 100 for fully
+      --transparent background. Values between 0-30 are typically most useful.
+      saga_winblend = 0,
+      -- when cursor in saga window you config these to move
+      move_in_saga = { prev = '<C-p>',next = '<C-n>'},
+      -- Error, Warn, Info, Hint
+      -- use emoji like
+      -- { "🙀", "😿", "😾", "😺" }
+      -- or
+      -- { "😡", "😥", "😤", "😐" }
+      -- and diagnostic_header can be a function type
+      -- must return a string and when diagnostic_header
+      -- is function type it will have a param `entry`
+      -- entry is a table type has these filed
+      -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
+      diagnostic_header = { " ", " ", " ", "ﴞ " },
+      -- preview lines of lsp_finder and definition preview
+      max_preview_lines = 10,
+      -- use emoji lightbulb in default
+      code_action_icon = "💡",
+      -- if true can press number to execute the codeaction in codeaction window
+      code_action_num_shortcut = true,
+      -- same as nvim-lightbulb but async
+      code_action_lightbulb = {
         enable = true,
+        enable_in_insert = true,
+        cache_code_action = true,
         sign = true,
-        sign_priority = 40,
+        update_time = 150,
+        sign_priority = 20,
         virtual_text = true,
       },
-      finder_definition_icon = "  ",
-      finder_reference_icon = "  ",
-      max_preview_lines = 10,
+      -- finder icons
+      finder_icons = {
+        def = '  ',
+        ref = '諭 ',
+        link = '  ',
+      },
+      -- finder do lsp request timeout
+      -- if your project big enough or your server very slow
+      -- you may need to increase this value
+      finder_request_timeout = 1500,
       finder_action_keys = {
         open = "o",
         vsplit = "v",
         split = "s",
+        tabe = "t",
         quit = "q",
-        scroll_down = "<C-f>",
-        scroll_up = "<C-b>",
       },
       code_action_keys = {
         quit = "q",
         exec = "<CR>",
       },
-      rename_action_keys = {
-        quit = "<C-c>",
-        exec = "<CR>",
+      definition_action_keys = {
+        edit = '<C-c>o',
+        vsplit = '<C-c>v',
+        split = '<C-c>s',
+        tabe = '<C-c>t',
+        quit = 'q',
       },
-      definition_preview_icon = "  ",
-      border_style = "single",
-      rename_prompt_prefix = "➤",
-      rename_output_qflist = {
-        enable = false,
-        auto_open_qflist = false,
+      rename_action_quit = "<C-c>",
+      rename_in_select = true,
+      -- show symbols in winbar must nightly
+      -- in_custom mean use lspsaga api to get symbols
+      -- and set it to your custom winbar or some winbar plugins.
+      -- if in_cusomt = true you must set in_enable to false
+      symbol_in_winbar = {
+        in_custom = false,
+        enable = true,
+        separator = ' ',
+        show_file = true,
+        -- define how to customize filename, eg: %:., %
+        -- if not set, use default value `%:t`
+        -- more information see `vim.fn.expand` or `expand`
+        -- ## only valid after set `show_file = true`
+        file_formatter = "",
+        click_support = false,
       },
+      -- show outline
+      show_outline = {
+        win_position = 'right',
+        --set special filetype win that outline window split.like NvimTree neotree
+        -- defx, db_ui
+        win_with = '',
+        win_width = 30,
+        auto_enter = true,
+        auto_preview = true,
+        virt_text = '┃',
+        jump_key = 'o',
+        -- auto refresh when change buffer
+        auto_refresh = true,
+      },
+      -- custom lsp kind
+      -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
+      custom_kind = {},
+      -- if you don't use nvim-lspconfig you must pass your server name and
+      -- the related filetypes into this table
+      -- like server_filetype_map = { metals = { "sbt", "scala" } }
       server_filetype_map = {},
-      diagnostic_prefix_format = "%d. ",
-      diagnostic_message_format = "%m %c",
-      highlight_prefix = false,
-    }
+    })
+
+
+    local u = require 'smithwebdev.core.utils'
+    local nnoremap = u.nnoremap
+    local vnoremap = u.vnoremap
+    local tnoremap = u.tnoremap
+
+    -- Lsp finder find the symbol definition implement reference
+    -- if there is no implement it will hide
+    -- when you use action in finder like open vsplit then you can
+    -- use <C-t> to jump back
+    nnoremap('gh', '<cmd>Lspsaga lsp_finder<CR>', { desc = 'LSP find symbol definition reference'})
+
+    -- Code action
+    nnoremap('<leader>ca', '<cmd>Lspsaga code_action<CR>', { desc = 'LSP code action'})
+    vnoremap('<leader>ca', '<cmd>Lspsaga code_action<CR>', { desc = 'LSP code action'})
+
+    -- Rename
+    nnoremap('gr', '<cmd>Lspsaga rename<CR>', { desc = 'LSP Rename'})
+
+    -- Peek Definition
+    -- you can edit the definition file in this flaotwindow
+    -- also support open/vsplit/etc operation check definition_action_keys
+    -- support tagstack C-t jump back
+    nnoremap('gd', '<cmd>LspSaga peek_definition<cr>', { desc = 'LSP peek definition'})
+
+    -- Show line diagnostics
+    nnoremap('<leader>cd',  '<cmd>Lspsaga show_line_diagnostics<CR>', { desc = 'LSP Show line diagnostic'})
+
+    -- Show cursor diagnostic
+    nnoremap('<leader>cd',  '<cmd>Lspsaga show_cursor_diagnostics<CR>', { desc = ''})
+
+    -- Diagnsotic jump can use `<c-o>` to jump back
+    nnoremap('[e',  '<cmd>Lspsaga diagnostic_jump_prev<CR>', { desc = ''})
+    nnoremap(']e',  '<cmd>Lspsaga diagnostic_jump_next<CR>', { desc = ''})
+
+    -- Only jump to error
+    nnoremap('[E', function() require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, { desc = ''})
+    nnoremap(']E', function() require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR }) end, { desc = ''})
+
+    -- Outline
+    nnoremap('<leader>o', '<cmd>LSoutlineToggle<CR>', { desc = ''})
+
+    -- Hover Doc
+    nnoremap('K', '<cmd>Lspsaga hover_doc<CR>', { desc = ''})
+
+    -- Float terminal
+    nnoremap('<A-d>', '<cmd>Lspsaga open_floaterm<CR>', { desc = ''})
+
+    -- if you want pass somc cli command into terminal you can do like this
+    -- open lazygit in lspsaga float terminal
+    -- nnoremap('<A-d>', '<cmd>Lspsaga open_floaterm lazygit<CR>', { desc = ''})
+
+    -- close floaterm
+    tnoremap('<A-d>', [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { desc = ''})
   end
 }
 

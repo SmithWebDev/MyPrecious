9a46a4b5783219fe614e284fe91ff8b25846e3a1
Darrick Smith <smithwebdd@gmail.com>
2022-10-25 14:25:59 -0400

    Added LspConfig

--- nvim-0.8/lua/smithwebdev/plugins/init.lua
+++ nvim-0.8/lua/smithwebdev/plugins/init.lua
@@ -51,6 +51,7 @@ local plugins = {
 
   -- LSP --
   'lsp_lines',          -- https://github.com/Maan2003/lsp_lines.nvim
+  'lspconfig',          -- https://github.com/neovim/nvim-lspconfig
   'lspsaga',            -- https://github.com/tami5/lspsaga.nvim
 
 

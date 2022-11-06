838759e80b6d50bf4a1e451670989d53aabfe8a5
Darrick Smith <smithwebdd@gmail.com>
2022-11-06 11:04:31 -0500

    Updated vimwiki snippets for conventional commits

--- nvim-0.8/lua/smithwebdev/snippets/vimwiki.lua
+++ nvim-0.8/lua/smithwebdev/snippets/vimwiki.lua
@@ -25,4 +25,29 @@ return {
     **Test Notes**
     ]], {i(1), i(2), i(3), i(4), i(5), i(6), i(7), i(8), })
   ),
+  s({
+    trig = 'com',
+    name = 'Conventional Commits',
+    dscr = 'Commit message structure'
+  },
+    fmt([[
+    {}: {}
+    ]],
+      {
+        c(1,{
+          t('Feature'),
+          t('Fix'),
+          t('Docs'),
+          t('Style'),
+          t('Refactor'),
+          t('Performance'),
+          t('Test'),
+          t('Build'),
+          t('CI'),
+          t('Chore'),
+          t('Revert')
+        }),
+        i(2)
+      })
+  ),
 }

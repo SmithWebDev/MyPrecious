8c8183b68704ebfbe8c767adaad9467bdd01944c
Darrick Smith <smithwebdd@gmail.com>
2022-11-26 18:52:38 -0500

    Added keybind to Vista

--- nvim-0.8/lua/smithwebdev/plugins/configs/vista.lua
+++ nvim-0.8/lua/smithwebdev/plugins/configs/vista.lua
@@ -4,7 +4,10 @@ M.plugin = {
   'liuchengxu/vista.vim', -- https://github.com/liuchengxu/vista.vim
 
   config = function()
+    local u = require 'smithwebdev.core.utils'
+    local nnoremap = u.nnoremap
 
+    nnoremap('<leader><leader>v', '<cmd>Vista!!<CR>', { desc = 'Open Vista window showing Tags in buffer'})
   end
 }
 

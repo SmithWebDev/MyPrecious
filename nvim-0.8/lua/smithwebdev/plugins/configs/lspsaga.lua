1abb7c7f1566d2cea5511f96b78131de78f35fda
Darrick Smith <smithwebdd@gmail.com>
2023-01-14 22:04:32 -0500

    Updated lspsaga to resolve Breaking Change

--- nvim-0.8/lua/smithwebdev/plugins/configs/lspsaga.lua
+++ nvim-0.8/lua/smithwebdev/plugins/configs/lspsaga.lua
@@ -5,7 +5,7 @@ M.plugin = {
 
   config = function()
     local lspsaga = require 'lspsaga'
-    lspsaga.init_lsp_saga({
+    lspsaga.setup({
       -- Options with default value
       -- "single" | "double" | "rounded" | "bold" | "plus"
       border_style = "single",

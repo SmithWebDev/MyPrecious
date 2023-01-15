1264e6db39ff5aff1233bc3c7239a5cc6f736e7b
Darrick Smith <smithwebdd@gmail.com>
2023-01-14 22:03:44 -0500

    Added new theme options

--- nvim-0.8/lua/smithwebdev/theme.lua
+++ nvim-0.8/lua/smithwebdev/theme.lua
@@ -5,17 +5,22 @@
 --local theme = 'gloombuddy'
 --local theme = 'bluedrake'
 --local theme = 'falcon'
-local theme = 'oxocarbon-lua'
+--local theme = 'oxocarbon-lua'
 --local theme = 'Sakura'
 --local theme = 'industry'
 --local theme = 'SerialExperimentsLain'
 --local theme = 'northpole'
+--local theme = 'southwest-fog'
+local theme = 'underwater'
 
 local ok, err = pcall(vim.cmd, 'colorscheme ' .. theme)
 
 if not ok then
   vim.cmd('colorscheme default')
 else
+--  vim.api.nvim_set_hl(0, "Normal", { bg= 'none'})
+--  vim.api.nvim_set_hl(0, "NormalFloat", { bg= 'none'})
+--  vim.api.nvim_set_hl(0, "cursorline", { bg= 'none'})
   print "Theme Loaded"
 end
 

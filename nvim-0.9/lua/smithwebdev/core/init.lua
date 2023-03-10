0ea276c3f0dc0ab66d14a619296f476e8b032256
Darrick Smith <smithwebdd@gmail.com>
2023-03-10 00:39:39 -0500

    Changed order of files loading

--- nvim-0.9/lua/smithwebdev/core/init.lua
+++ nvim-0.9/lua/smithwebdev/core/init.lua
@@ -1,4 +1,3 @@
+require 'smithwebdev.core.settings'
 require 'smithwebdev.core.autocmds'
 require 'smithwebdev.core.keybinds'
-require 'smithwebdev.core.settings'
-require 'smithwebdev.core.plugins'

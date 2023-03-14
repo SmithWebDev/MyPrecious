c091fbdabda2e350ecfa1169b1e1a7b2ce664766
Darrick Smith <smithwebdd@gmail.com>
2023-03-13 20:54:36 -0400

    Updated Quick Close autocmd to accept multiple filetypes

--- nvim-0.9/lua/smithwebdev/core/autocmds.lua
+++ nvim-0.9/lua/smithwebdev/core/autocmds.lua
@@ -1,16 +1,26 @@
 ---------------------------------------------------------------------------------
 local Quick_Close = vim.api.nvim_create_augroup('Quick_Close', { clear = true })
-vim.api.nvim_create_autocmd('BufEnter', {
-  callback = function()
-    if vim.bo.buftype == 'lspinfo' then
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.keymap.set('n', 'q', ':q<CR>', { silent = true, buffer = bufnr })
-    end
-  end,
-  group = Quick_Close,
-  desc = "close LspInfo window with 'q'",
-})
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = 'Quick_Close',
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "query", -- :InspectTree
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", ":q<cr>", { buffer = event.buf, silent = true })
+  end,
+})
 --------------------------------------------------------------------------------
 local AutoSaveGroup = vim.api.nvim_create_augroup('autosave_user_events', { clear = true })
 vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
@@ -20,8 +30,18 @@ vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
     local ft = vim.bo[bufnr].filetype
     local modifiable = vim.bo[bufnr].modifiable
     local is_empty_buftype = vim.bo[bufnr].buftype == ''
-    local ignorelist = { 'packer', 'netrw', 'TelescopePrompt', 'lspinfo', 'lsp-installer', 'query', 'tsplayground',
-      'text', 'harpoon', 'scratch' }
+    local ignorelist = {
+      'packer',
+      'netrw',
+      'TelescopePrompt',
+      'lspinfo',
+      'lsp-installer',
+      'query',
+      'tsplayground',
+      'text',
+      'harpoon',
+      'scratch'
+    }
     if not vim.tbl_contains(ignorelist, ft) and modifiable and is_empty_buftype then
       vim.cmd 'silent update!'
     end
